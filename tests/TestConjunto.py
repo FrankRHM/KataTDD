73e5b1a0e05992ade7a8d62a10e5c90f137366bb
FrankRHM <gilvonio.franklin@gmail.com>
2022-12-03 11:29:31 -0500

    prueba

---
+++ tests/TestConjunto.py
@@ -0,0 +1,19 @@
+import unittest
+from src.logica.Conjunto import Conjunto
+
+class TestConjunto(unittest.TestCase):
+     def test_conjunto_vacio_retornaNone(self):
+          conjunto=Conjunto([])
+          self.assertIsNone(conjunto.promedio())
+
+     def test_conjunto_unElemento_RetornaValorUnicoElemento(self):
+          conjunto = Conjunto([5])
+          self.assertEqual(5, conjunto.promedio())
+
+     def test_conjunto_dosElementos_RetornaPromedioElementos(self):
+          conjunto = Conjunto([5, 7])
+          self.assertEqual(6, conjunto.promedio())
+
+     def test_conjunto_nElementos_RetornaPromedioNElementos(self):
+          conjunto = Conjunto([2, 4, 8, 9, 10, 15])
+          self.assertEqual((2 + 4 + 8 + 9 + 10 + 15)/6, conjunto.promedio())
